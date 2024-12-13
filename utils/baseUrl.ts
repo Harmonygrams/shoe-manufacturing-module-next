8b232a9f0fda2346adfcad784dfe1136139e6b95
Harmony <user@USERs-MacBook-Pro.local>
2024-12-13 12:06:29 +0100

    added baseurl

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -1,4 +1,4 @@
 export function baseUrl () {
-    // return "https://shoe-manufacturing-module-node.onrender.com/api/v1"
-    return "http://localhost:5001/api/v1"
+    return "https://shoe-manufacturing-module-node.onrender.com/api/v1"
+    // return "http://localhost:5001/api/v1"
 }
